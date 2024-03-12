ce50789ced8bb69b335cee9553fae19e016d0751
Zahrafov <evolutionzav@gmail.com>
2024-03-12 21:26:12 +0100

    my commit

--- src/Task5/Main.java
+++ src/Task5/Main.java
@@ -1,5 +1,10 @@
 package Task5;
 
+// Використовуючи Intelij IDEA, створити проект, пакет.
+// Створити клас MyArea, в ньому оголосити константу PI = 3.14 та статичний метод areaOfCircle,
+// який повинен приймати радіус та використовуючи PI порахувати площу кола.
+// Створити клас Main, де запустити цей метод.
+
 public class Main {
     public static void main(String[] args) {
         double radius = 30;
