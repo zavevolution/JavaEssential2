9c82c894566d4715bb496d9724536f77e597516d
Zahrafov <evolutionzav@gmail.com>
2024-03-24 20:05:30 +0100

    my commit

--- src/dz6/task4/VehiclesTest.java
+++ src/dz6/task4/VehiclesTest.java
@@ -6,5 +6,34 @@ package dz6.task4;
 // який повертає рядок з кольором автомобіля, і містити перевантажений метод toString(),
 // який повинен повертати рядок з назвою екземпляра, кольором та вартістю автомобіля.
 
+enum Vehicles {
+    BMW("red", 50000),
+    Volkswagen("white", 36000),
+    Opel("gray", 34000),
+    Mercedes("black", 60000);
+    private String color;
+    private int price;
+
+    Vehicles(String color, int price) {
+        this.color = color;
+        this.price = price;
+    }
+
+    String getColor() { return color; }
+
+    @Override
+    public String toString() {
+        return "\nАвтомобіль: " +
+                name() +
+                "\nколір: " + color +
+                "\nціна: " + price;
+    }
+}
+
 public class VehiclesTest {
+    public static void main(String[] args) {
+        Vehicles vehicle = Vehicles.Mercedes;
+        System.out.println(vehicle.getColor());
+        System.out.println(vehicle);
+    }
 }
