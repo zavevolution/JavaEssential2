59c4755d81f90e89d1149bf4e21595d577faf79a
Zahrafov <evolutionzav@gmail.com>
2024-03-24 19:37:07 +0100

    my commit

---
+++ src/dz6/task2/AnimalsTest.java
@@ -0,0 +1,34 @@
+package dz6.task2;
+
+// Створіть проект за допомогою IntelliJ IDEA.
+// Створіть перерахунковий тип (enum) Animals, що містить конструктор,
+// який повинен набувати цілого числа (вік тварини), і містити перевантажений метод toString(),
+// який повинен повертати назву екземпляра та вік тварини.
+
+enum Animals {
+    Dog(3), Cat(5), Cow(7);
+    private int age;
+
+    Animals(int age) {
+        this.age = age;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Animals{" +
+                name() +
+                " age=" + age +
+                '}';
+    }
+}
+
+public class AnimalsTest {
+    public static void main(String[] args) {
+        Animals animals = Animals.Cow;
+        System.out.println(animals);
+    }
+}
