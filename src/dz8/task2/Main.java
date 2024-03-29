f68154ba7e73aca6746ae2fca654f46e7873f192
Zahrafov <evolutionzav@gmail.com>
2024-03-29 21:25:33 +0100

    my commit

--- src/dz8/task2/Main.java
+++ src/dz8/task2/Main.java
@@ -2,13 +2,32 @@ package dz8.task2;
 
 public class Main {
     public static void main(String[] args) {
-        Device device = new Device("Samsung", 120, "AB1234567CD");
+        Device device1 = new Device("Samsung", 120, "AB1234567CD");
+        Device device2 = new Device("Samsung", 120, "AB1234567CD");
+        Device device3 = new Device("Xiaomi", 110, "XM1234567CD");
+        Device device4 = device3;
         Monitor monitor = new Monitor("Samsung", 120, "AB1234567CD", 1280, 1024);
         EthernetAdapter ethernetAdapter = new EthernetAdapter("Samsung", 90, "FK384757BN", 198, "MAC:73943949");
 
-
-        System.out.println(device);
+        System.out.println();
+        System.out.println(device1);
+        System.out.println(device2);
+        System.out.println(device3);
+        System.out.println(device4);
         System.out.println(monitor);
         System.out.println(ethernetAdapter);
+
+        System.out.println();
+        System.out.println("device1.equals(device2): " + device1.equals(device2));
+        System.out.println("device1.equals(device3): " + device1.equals(device3));
+        System.out.println("device4.equals(device3): " + device4.equals(device3));
+
+        System.out.println();
+        System.out.println("hashCode device1: " + device1.hashCode());
+        System.out.println("hashCode device2: " + device2.hashCode());
+        System.out.println("hashCode device3: " + device3.hashCode());
+        System.out.println("hashCode device4: " + device4.hashCode());
+        System.out.println("hashCode monitor: " + monitor.hashCode());
+        System.out.println("hashCode ethernetAdapter: " + ethernetAdapter.hashCode());
     }
 }
