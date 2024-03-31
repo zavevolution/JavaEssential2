9bea45b34c27dbf0db34ad03a82063c41d86a781
Zahrafov <evolutionzav@gmail.com>
2024-03-31 18:26:06 +0200

    my commit

--- src/dz9/task5/MyClass.java
+++ src/dz9/task5/MyClass.java
@@ -5,11 +5,15 @@ package dz9.task5;
 // який породжуватиме екземпляри типу, зазначеного як параметр типу (покажчика місця заповнення типом – Т).
 
 public class MyClass {
-    public static <T> T factoryMethod() {
-        return (T)(new Object());
+    public static <T> T factoryMethod(Class<T> cl) {
+        try {
+            return cl.newInstance();
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     public static void main(String[] args) {
-        String str = MyClass.factoryMethod();
+        String str = MyClass.factoryMethod(String.class);
     }
 }
